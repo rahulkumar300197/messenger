e311dffea8a10a035ae252ec7a40f5bff06fc02b
Rahul Kumar <rahulkumar@rediffmail.com>
2018-01-16 00:57:57 +0530

    first commit

---
+++ app/src/main/java/com/example/rahul/messanger/MessageListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.rahul.messanger;
+
+/**
+ * Created by rahul on 15/1/18.
+ */
+
+public class MessageListAdapter {
+}
---
+++ app/src/main/java/com/example/rahul/messanger/UserMessage.java
@@ -0,0 +1,8 @@
+package com.example.rahul.messanger;
+
+/**
+ * Created by rahul on 16/1/18.
+ */
+
+public class UserMessage {
+}
